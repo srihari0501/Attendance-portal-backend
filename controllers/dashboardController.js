a2502206d98a3bcad1a14abf30a3e49f674adc88
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2024-06-16 03:06:47 +0530

    Update dashboardController.js

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -5,7 +5,8 @@ exports.getUserData = async (req, res) => {
     try {
         const userId = req.userId;
         console.log('uid' + userId);
-        const userData = await User.find(userId);
+        const userObjectId = mongoose.Types.ObjectId(userId);
+        const userData = await User.findById(userObjectId).populate('attendance').populate('leave');
         console.log('userData id' + userData);
         res.status(200).json(userData);
     } catch (error) {
