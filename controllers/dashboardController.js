a8acd7161ce0fa82da470555441c2067258f7ecd
srihari0501 <112402921+srihari0501@users.noreply.github.com>
2024-06-16 01:18:49 +0530

    Update dashboardController.js
    updated

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,7 +4,7 @@ const Attendance = require('../models/Attendance');
 exports.getUserData = async (req, res) => {
     try {
         const userId = req.userId;
-        const userData = await User.findById('666a9739b4986652cc64db0d').populate('attendance').populate('leave');
+        const userData = await User.findById(userId).populate('attendance').populate('leave');
 
         res.status(200).json(userData);
     } catch (error) {
